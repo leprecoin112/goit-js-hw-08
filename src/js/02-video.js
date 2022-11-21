f35c0a6eab233adaaf78bd278797ed9e8a86dc63
Yaroslav Khakhuda <hahyda14@gmail.com>
2022-11-21 20:18:30 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import storage from './storage';
+import throttle from 'lodash.throttle';
 
+const VIDEO_PLAYER_KEY = 'videoplayer-current-time';
+
+const videoPlayerEl = document.querySelector('#vimeo-player');
+const player = new Player(videoPlayerEl);
+
+player.on('play', function () {
+  setCurrentTimePlay(storage.load(VIDEO_PLAYER_KEY));
+});
+
+player.on('timeupdate', throttle(currentTimePlay, 1000));
+
+function currentTimePlay(e) {
+  storage.save(VIDEO_PLAYER_KEY, e);
+}
+
+function setCurrentTimePlay(time) {
+  if (time) {
+    player.setCurrentTime(time.seconds);
+  }
+}
